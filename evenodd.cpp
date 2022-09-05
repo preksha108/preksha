1bbf946286b019a5b1d9f13934b2efac74332d1d
preksha108 <104293272+preksha108@users.noreply.github.com>
2022-09-05 09:35:37 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cin>>i;
+    if (i%2==0)
+        cout <<"even number";
+    else
+        cout<<"odd number";
+
+        return 0;
+}
